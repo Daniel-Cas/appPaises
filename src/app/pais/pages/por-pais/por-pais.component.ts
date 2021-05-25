d70ee261c81bfabb8c4a281fe2beb062a2c8da58
Daniel-Cas <dmattcomics@gmail.com>
2021-05-24 22:39:50 -0500

    Creacion del Proyecto y mapeo hechos

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+
+  termino: string = 'El pepe';
+
+
+
+
+  constructor() { }
+
+ 
+  buscar() {
+    console.log(this.termino);
+  }
+
+}
